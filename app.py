4cdef2744a8260d4ff2c2d295cd701422044470e
Auto-Tools <113418884+GFGUser01@users.noreply.github.com>
2023-05-31 16:56:26 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,91 @@
+import streamlit as st
+# import module
+import requests
+from bs4 import BeautifulSoup
+import csv
+import re
+import pandas as pd
+
+
+# def add_numbers(num1, num2):
+#     return num1 + num2
+
+def scrapper( tag, Page):
+    def getdata(url):
+        r = requests.get(url)
+        return r.text
+    
+    
+    
+    Title = []
+    view = []
+    tag = "python"
+    page = 3
+    
+    for i in range(Page):
+        st.success(f"Processing page : {i}", icon="✅")
+        # url
+        # url = "https://stackoverflow.com/questions/tagged/"+tag+"/?tab=votes&page="+str(page)
+        url= "https://stackoverflow.com/questions/tagged/"+tag+"?tab=votes&page="+str(i)
+        
+
+        # pass the url
+        # into getdata function
+        htmldata = getdata(url)
+        soup = BeautifulSoup(htmldata, 'html.parser')
+
+        All = []
+        # traverse author name
+        for i in soup.find_all("h3", "s-post-summary--content-title"):
+            All.append(i.get_text().split("\n"))
+        View = []
+        for i in soup.find_all("div", "s-post-summary--stats-item is-supernova"):
+            View.append(i.get_text().split("\n"))
+
+        for i in All:
+            Title.append(i[-2])
+        for i in View:
+            view.append(i[1])
+    if view!=Title:
+        temp = len(view)-len(Title)
+        for i in range(abs(temp)):
+            view.append("NA")
+
+    dic = {"Title": Title, "View":view}
+    df = pd.DataFrame(dic) 
+    
+    
+    @st.cache
+    def convert_df_to_csv(df):
+      # IMPORTANT: Cache the conversion to prevent computation on every rerun
+      return df.to_csv().encode('utf-8')
+
+
+    st.download_button(
+      label="Download data as CSV",
+      data=convert_df_to_csv(df),
+      file_name='large_df.csv',
+      mime='text/csv',
+    )
+    
+    return("Hurrah!!!!!!! Completed")
+
+
+def main():
+    st.title("Scrapper App[Stackoverflow]")
+    st.write("Enter Tag and Number of pages to get the Result.")
+
+    tag = st.text_input("Category: ")
+    Page = st.number_input("Number of Pages :", step=1)
+
+    if st.button("Export to CSV"):
+        result = scrapper( tag, Page)
+        st.success(result)
+
+
+if __name__ == "__main__":
+    main()
+
+    
+
+ 
