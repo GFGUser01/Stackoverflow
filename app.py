32f49641f3275debf803d276bcc6396e24ca2a2a
Satyam <113418884+GFGUser01@users.noreply.github.com>
2024-02-07 15:14:55 +0530

    Update app.py
    Check

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-
-
 import streamlit as st
 # import module
 import requests
@@ -7,11 +5,8 @@ from bs4 import BeautifulSoup
 import csv
 import re
 import pandas as pd
-
-
 # def add_numbers(num1, num2):
 #     return num1 + num2
-
 def scrapper( tag, Page):
     def getdata(url):
         r = requests.get(url)
@@ -26,15 +21,25 @@ def scrapper( tag, Page):
     for i in range(Page):
         st.success(f"Processing page : {i}", icon="✅")
         # url
-        url = "https://stackoverflow.com/questions/tagged/"+tag+"/?sort=MostVotes&page="+str(page)
-        # url= "https://stackoverflow.com/questions/tagged/"+tag+"?tab=Newest&page="+str(i)
+        # url = "https://stackoverflow.com/questions/tagged/"+tag+"/?tab=votes&page="+str(page)
+        url= "https://stackoverflow.com/questions/tagged/"+tag+"?tab=votes&page="+str(i)
         print(url)
 
         # pass the url
+
+    
+          
+            
+    
+
+          
+          Expand Down
+    
+    
+  
         # into getdata function
         htmldata = getdata(url)
         soup = BeautifulSoup(htmldata, 'html.parser')
-
         All = []
         # traverse author name
         for i in soup.find_all("h3", "s-post-summary--content-title"):
@@ -42,7 +47,6 @@ def scrapper( tag, Page):
         View = []
         for i in soup.find_all("div", "s-post-summary--stats-item is-supernova"):
             View.append(i.get_text().split("\n"))
-
         for i in All:
             Title.append(i[-2])
         for i in View:
@@ -51,7 +55,6 @@ def scrapper( tag, Page):
         temp = len(view)-len(Title)
         for i in range(abs(temp)):
             view.append("NA")
-
     dic = {"Title": Title, "View":view}
     df = pd.DataFrame(dic) 
     
@@ -60,8 +63,6 @@ def scrapper( tag, Page):
     def convert_df_to_csv(df):
       # IMPORTANT: Cache the conversion to prevent computation on every rerun
       return df.to_csv().encode('utf-8')
-
-
     st.download_button(
       label="Download data as CSV",
       data=convert_df_to_csv(df),
@@ -70,32 +71,20 @@ def scrapper( tag, Page):
     )
     
     return("Hurrah!!!!!!! Completed")
-
-
 def main():
     st.title("Scrapper App[Stackoverflow]")
     st.write("Enter Tag and  of pages to get the Result.")
-
     tag = st.text_input("Category: ")
     Page = st.number_input("Number of Pages :", step=1)
     
     val = st.text_input("User: ")
     users = ["user404", "Satyam_"]
-
     if st.button("Export to CSV"):
-
         if val in users:
             result = scrapper( tag, Page)
             st.success(result)
         else:
             st.success(f"Invalid Users : {val}", icon="🔴")
-
-
-
 if __name__ == "__main__":
     main()
-
     
-
- 
-
